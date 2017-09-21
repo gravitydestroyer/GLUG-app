f739f0820d3d71bf543e157b4983b14e4a3ead44
gravitydestroyer <aritra240@gmail.com>
2017-09-21 13:51:42 +0530

    fixed faq page

---
+++ app/src/main/java/com/gnulinuxusersgroup/nitdgp/glug/firebase.java
@@ -0,0 +1,13 @@
+package com.gnulinuxusersgroup.nitdgp.glug;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class firebase extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+    }
+}
